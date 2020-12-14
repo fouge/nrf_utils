0bb8fb65dbb3c4d07f8d3d2aa262c1fb7b7863bc
Cyril Fougeray <cyril.fougeray@gmail.com>
2020-12-14 12:47:59 +0100

    Some cleaning of cmake facilitators

--- code/cmake/utils.cmake
+++ code/cmake/utils.cmake
@@ -22,15 +22,15 @@ endmacro()
 # Add custom command to print firmware size in Berkley format
 function(firmware_size target)
     add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND ${CMAKE_SIZE_UTIL} -B
-        "${CMAKE_CURRENT_BINARY_DIR}/${target}${CMAKE_EXECUTABLE_SUFFIX}"
-    )
+            COMMAND ${CMAKE_SIZE_UTIL} -B
+            "${CMAKE_CURRENT_BINARY_DIR}/${target}${CMAKE_EXECUTABLE_SUFFIX}"
+            )
 endfunction()
 
 # Add a command to generate firmare in a provided format
 function(generate_object target suffix type)
     add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND ${CMAKE_OBJCOPY} -O${type}
-        "${CMAKE_CURRENT_BINARY_DIR}/${target}${CMAKE_EXECUTABLE_SUFFIX}" "${CMAKE_CURRENT_BINARY_DIR}/${target}${suffix}"
-    )
+            COMMAND ${CMAKE_OBJCOPY} -O${type}
+            "${CMAKE_CURRENT_BINARY_DIR}/${target}${CMAKE_EXECUTABLE_SUFFIX}" "${CMAKE_CURRENT_BINARY_DIR}/${target}${suffix}"
+            )
 endfunction()
