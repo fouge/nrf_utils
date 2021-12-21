4233db0bac481bc323609d590412b8010f685e0f
Cyril Fougeray <cyril.fougeray@gmail.com>
2021-12-21 16:12:34 +0100

    Some CMake refacto

--- code/cmake/version.cmake
+++ code/cmake/version.cmake
@@ -1,8 +1,9 @@
-function(generate_version ini_path output_path)
-    get_filename_component(PYTHON_SCRIPT_PATH ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../version/gen_version.py ABSOLUTE)
+get_filename_component(VERSION_PY_PATH ${CMAKE_CURRENT_LIST_DIR}/../version/gen_version.py ABSOLUTE)
+
+function(version_deps ini_path output_path)
     add_custom_command(
             OUTPUT ${output_path}
-            COMMAND python ${PYTHON_SCRIPT_PATH} -f ${ini_path} -i ${output_path}
+            COMMAND python ${VERSION_PY_PATH} -f ${ini_path} -i ${output_path}
             DEPEND ${ini_path}
     )
 endfunction()
