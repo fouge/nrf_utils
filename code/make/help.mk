e1dbd7e832d7cc5aa6d2f10101117e964ce7ad60
Cyril Fougeray <cyril.fougeray@gmail.com>
2022-03-01 11:44:20 +0100

    Add help target with colorful help message

---
+++ code/make/help.mk
@@ -0,0 +1,35 @@
+# Print colorful help using: make help
+#
+# Set help for your targets with double-#:
+#	## this help will be printed out
+#	target_example:
+#		@echo "This is an example"
+#
+# See below the help target
+# To shift help sentences, set TARGET_MAX_CHAR_NUM before including this file
+
+# COLORS
+GREEN  := $(shell tput -Txterm setaf 2)
+YELLOW := $(shell tput -Txterm setaf 3)
+WHITE  := $(shell tput -Txterm setaf 7)
+RESET  := $(shell tput -Txterm sgr0)
+
+# Default to 25 col
+TARGET_MAX_CHAR_NUM?=20
+
+## Show this help
+help:
+	@echo ''
+	@echo 'Usage:'
+	@echo '  ${YELLOW}make${RESET} ${GREEN}<target>${RESET}'
+	@echo ''
+	@echo 'Targets:'
+	@awk '/^[a-zA-Z\-\_0-9]+:/ { \
+		helpMessage = match(lastLine, /^## (.*)/); \
+		if (helpMessage) { \
+			helpCommand = substr($$1, 0, index($$1, ":")-1); \
+			helpMessage = substr(lastLine, RSTART + 3, RLENGTH); \
+			printf "  ${YELLOW}%-$(TARGET_MAX_CHAR_NUM)s${RESET} ${GREEN}%s${RESET}\n", helpCommand, helpMessage; \
+		} \
+	} \
+	{ lastLine = $$0 }' $(MAKEFILE_LIST)
